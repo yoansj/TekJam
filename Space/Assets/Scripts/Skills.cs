2642038063ff336080adfab1a0b72df40018fbea
Yoan SAINT JUSTE <48960798+yoansj@users.noreply.github.com>
2020-02-22 15:01:05 +0100

    [MOD] Skills file

--- Space/Assets/Scripts/Skills.cs
+++ Space/Assets/Scripts/Skills.cs
@@ -7,11 +7,17 @@ public class Skills : MonoBehaviour
 {
     private PlayerMovement playerMovement;
 
+    [Header("Lumiére")]
+    private Light playerLight;
+    public float maxIntesity = 2;
+    public float maxRange = 2;
+
     public int level = 0;
     // Start is called before the first frame update
     void Start()
     {
-        playerMovement = GetComponentInParent<PlayerMovement>();
+        playerLight = GetComponentInChildren<Light>();
+        playerMovement = GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
@@ -22,17 +28,32 @@ public class Skills : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Z))
             Shrink();
+
+        if (Input.GetKey(KeyCode.E))
+            Enlighten();
+        else
+        {
+            playerLight.intensity = 0;
+            playerLight.range = 0;
+        }
     }
 
     private void Grow()
     {
         playerMovement.rb.transform.localScale += new Vector3(1, 1, 0);
     }
+
     private void Shrink()
     {
         if (playerMovement.rb.transform.localScale.x != 1)
         {
             playerMovement.rb.transform.localScale += new Vector3(-1, -1, 0);
         }
+    }
+
+    private void Enlighten()
+    {
+        playerLight.intensity = maxIntesity;
+        playerLight.range = maxRange;
     }
 }
