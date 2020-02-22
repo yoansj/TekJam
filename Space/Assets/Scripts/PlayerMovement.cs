76119f5c813670bc6b78e46bf2a7b625314a1897
Yoan SAINT JUSTE <48960798+yoansj@users.noreply.github.com>
2020-02-22 09:58:56 +0100

    Pushed old files into new repo

---
+++ Space/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpSpeed = 10f;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            Jump();
+
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, 0) * Time.deltaTime * moveSpeed;
+    }
+
+    void Jump()
+    {
+        rb.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+    }
+}
