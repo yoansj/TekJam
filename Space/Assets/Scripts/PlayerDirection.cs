6f280ac563c6b24470a7a2f6e6e863d5af6e84a7
SenaYora <matthieu.veiga@epitech.eu>
2020-02-22 12:07:48 +0100

    Add camera movement + following character + Effect when change side

---
+++ Space/Assets/Scripts/PlayerDirection.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDirection : MonoBehaviour
+{
+    public enum direction_e {
+        LEFT = -1,
+        RIGHT = 1,
+        UNDEFINED = 0,
+    };
+
+    private direction_e playerDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerDir = direction_e.UNDEFINED;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            setPlayerDir(direction_e.LEFT);
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            setPlayerDir(direction_e.RIGHT);
+    }
+
+    void setPlayerDir(direction_e dir)
+    {
+        playerDir = dir;
+    }
+
+    public direction_e getPlayerDir()
+    {
+        return (playerDir);
+    }    
+}
